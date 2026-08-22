bacc2592210e3483a0dbb89d76c83462114bb556
Bangdayeon <dybang00@gmail.com>
2026-08-22 14:57:45 +0900

    common checkbox component

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,5 +1,6 @@
 export { Button } from "./Button/Button";
 export { Input } from "./Input/Input";
+export { Checkbox } from "./Checkbox/Checkbox";
 export { Modal } from "./Modal/Modal";
 export { Toast } from "./Toast/Toast";
 export { Dropdown } from "./Dropdown/Dropdown";
